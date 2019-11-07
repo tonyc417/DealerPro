5c4675c4b45146e238b86f5b94b8ecf97c61292d
unknown <tonycruz417@gmail.com>
2019-11-06 19:32:03 -0600

    working on front end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <h2>Welcome to the Trades Portal</h2>
+        <h3>Please fill out the following information</h3>
+    </div>
+  );
+}
+
+export default App;
